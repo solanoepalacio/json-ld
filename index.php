876a599fd929e213dfe082af75e2371f039240b0
Solano Palacio <solano@magoya.com>
2019-05-10 10:24:42 -0300

    minor improv

--- index.php
+++ index.php
@@ -41,8 +41,7 @@
     <section>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/WAbMnu55lys" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <div>
-            <h2>Title</h2>
-            <p>Top 10 FASTEST Cars in the World 2016 (Top trends)</p>
+            <h2>Top 10 FASTEST Cars in the World 2016 (Top trends)</h2>
         </div>
         <div>
             <h3>Description</h3>
