7a3aa4e156a0b51c97f022cc76f7804eb171993c
Solano Palacio <solano@magoya.com>
2019-05-21 17:42:05 -0300

    add remaining links to menu

--- includes/menu.php
+++ includes/menu.php
@@ -2,13 +2,13 @@
     <p>Here you'll find a video about a fast car or something quite similar!</p>
     <p>You can see slightly different forms to load the json-ld for this data by choosing of this list:</p>
     <ul>
-        <li><a href="/index.php" >Load everything statically</a></li>
+        <li><a href="/index.php" >Load a youtube embed video and json-ld statically</a></li>
         <li><a href="/no-transcript.php" >Load the video and json-ld statically</a></li>
         <li><a href="content-loaded.php">Load the json-ld on dom content loaded</a></li>
         <li><a href="content-transcript-loaded.php">Load the json-ld & the transcript on dom content loaded</a></li>
         <li><a href="timeout.php">Load the json-ld after 10 seconds with a timeout</a></li>
         <li><a href="timeout-transcript.php">Load the json-ld & the transcript after 10 seconds with a timeout</a></li>
-
+        <!-- so far, so food -->
         <li><a href="network.php">Load json-ld using a network request</a></li>
         <li><a href="network-transcript.php"></a>Load json-ld & the transcript using a network request</li>
 
@@ -17,6 +17,8 @@
 
         <li><a href="content-loaded-src.php">Load json-ld adding a script element after dom content is loaded</a></li>
         <li><a href="content-loaded-src-transcript.php">Load json-ld and transcript adding a script element after dom content is loaded</a></li>
+        <li><a href="/vimeo-static.php">Load a Vimeo embed and json-ld statically</a></li>
+        <li><a href="/video-js-static.php">Load a video-js player embed and a json-ld statically</a></li>
 
         <li><a href="vwrap.php">Load the vwrap as is</a></li>
     </ul>
