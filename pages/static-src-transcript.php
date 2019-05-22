0a9a5e6e2520f61601092f4957f1b6ac149d1dd0
Solano Palacio <solano@magoya.com>
2019-05-22 10:29:39 -0300

    add static src transcript pages

---
+++ pages/static-src-transcript.php
@@ -0,0 +1,28 @@
+<?php
+    $viewName = 'JSON-LD loaded with a static script tag (with-transcript)';
+    $transcriptHTML = file_get_contents('../includes/transcript.html');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include '../includes/title.php' ?>
+    <script src="/assets/json-ld" ></script>
+</head>
+<body>
+    <?php include '../includes/header.php' ?>
+    <?php include '../includes/menu.php' ?>
+    <?php include '../includes/video.php' ?>
+
+    <script>
+        (function insertVideoHtmlTranscript () {
+            console.log('Adding Html Transcript');
+            const transcript = document.createElement('div');
+            transcript.innerHTML = `<?php echo $transcriptHTML; ?>`;
+            const body = document.querySelector('body');
+            body.insertBefore(transcript, body.lastChild);
+        })()
+    </script>
+</body>
+</html>
---
+++ pages/static-src.php
@@ -0,0 +1,18 @@
+<?php
+    $viewName = 'JSON-LD loaded with a static script tag (no-transcript)';
+    $transcriptHTML = file_get_contents('../includes/transcript.html');
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include '../includes/title.php' ?>
+    <script src="/assets/json-ld" ></script>
+</head>
+<body>
+    <?php include '../includes/header.php' ?>
+    <?php include '../includes/menu.php' ?>
+    <?php include '../includes/video.php' ?>
+</body>
+</html>
