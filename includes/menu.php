9dcb98e5b3110086aeb27b78f9cbd529a64cfed6
Solano Palacio <solano@magoya.com>
2019-05-22 11:51:08 -0300

    improve menu names

--- includes/menu.php
+++ includes/menu.php
@@ -2,28 +2,28 @@
     <p>Here you'll find a video about a fast car or something quite similar!</p>
     <p>You can see slightly different forms to load the json-ld for this data by choosing of this list:</p>
     <ul>
-        <li><a href="/index.php" >Load a youtube embed video and json-ld statically</a></li>
-        <li><a href="/pages/no-transcript.php" >Load the video and json-ld statically</a></li>
+        <li><a href="/index.php" >[Youtube] - Load statically - with transcript</a></li>
+        <li><a href="/pages/no-transcript.php" >[Youtube] - Load statically</a></li>
 
-        <li><a href="/pages/content-loaded.php">Load the json-ld on dom content loaded</a></li>
-        <li><a href="/pages/content-transcript-loaded.php">Load the json-ld & the transcript on dom content loaded</a></li>
+        <li><a href="/pages/content-loaded.php">[Youtube] - Append to the dom on dom-content-loaded event</a></li>
+        <li><a href="/pages/content-transcript-loaded.php">[Youtube] - Append to the dom on dom-content-loaded event - with transcript</a></li>
 
-        <li><a href="/pages/timeout.php">Load the json-ld after 10 seconds with a timeout</a></li>
-        <li><a href="/pages/timeout-transcript.php">Load the json-ld & the transcript after 10 seconds with a timeout</a></li>
+        <li><a href="/pages/timeout.php">[Youtube] - Load after 10 seconds with a timeout</a></li>
+        <li><a href="/pages/timeout-transcript.php">[Youtube] - Loadafter 10 seconds with a timeout - with transcript</a></li>
 
-        <li><a href="/pages/network.php">Load json-ld using a network request</a></li>
-        <li><a href="/pages/network-transcript.php">Load json-ld & the transcript using a network request</a></li>
+        <li><a href="/pages/network.php">[Youtube] - Load over a network request</a></li>
+        <li><a href="/pages/network-transcript.php">[Youtube] - Load over a network request - with transcript</a></li>
 
-        <li><a href="/pages/static-src.php">Load json-ld using script element internal request machinery</a></li>
-        <li><a href="/pages/static-src-transcript.php">Load json-ld & transcript using internal request machinery</a></li>
+        <li><a href="/pages/static-src.php">[Youtube] - Load using an html script tag</a></li>
+        <li><a href="/pages/static-src-transcript.php">[Youtube] - Load using an html script tag - with transcript</a></li>
 
-        <li><a href="/pages/content-loaded-src.php">Load json-ld adding a script element after dom content is loaded</a></li>
-        <li><a href="/pages/content-loaded-src-transcript.php">Load json-ld and transcript adding a script element after dom content is loaded</a></li>
+        <li><a href="/pages/content-loaded-src.php">[Youtube] - Load using an html script tag after dom-content-loaded event</a></li>
+        <li><a href="/pages/content-loaded-src-transcript.php">[Youtube] - Load using an html script tag after dom-content-loaded event</a></li>
 
-        <li><a href="/pages/vimeo-static.php">Load a Vimeo embed and json-ld statically</a></li>
+        <li><a href="/pages/vimeo-static.php">[VIMEO] - Load statically</a></li>
 
-        <li><a href="/pages/video-js-static.php">Load a video-js player embed statically</a></li>
-        <li><a href="/pages/video-js-static-transcript.php">Load a video-js player embed and a json-ld statically</a></li>
+        <li><a href="/pages/video-js-static.php">[VIDEO - JS] Load statically</a></li>
+        <li><a href="/pages/video-js-static-transcript.php">[VIDEO - JS] Load statically - with transcript</a></li>
 
         <li><a href="/pages/vwrap.php">Load the vwrap as is</a></li>
     </ul>
